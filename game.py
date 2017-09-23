e59ecbe557fc83da591936fdbe4e64af122c245a
XtraRyce <nuevo.matt@gmail.com>
2017-09-24 04:42:21 +0800

    Added basic message display functions

--- game.py
+++ game.py
@@ -16,18 +16,24 @@ screen = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('TicTacTrivia')
 
 clock = pygame.time.Clock()
+font = pygame.font.SysFont(None, 25)
 
 def text_objects(text, color):
     textSurface = font.render(text, True, color)
     return textSurface, textSurface.get_rect()
 
 
-def message_to_screen(msg, color):
+def message_to_screen(msg, color, cell):
     textSurf, textRect = text_objects(msg, color)
-    # screen_text = font.render(msg, True, color)
-    # gameDisplay.blit(screen_text, [display_width/2, display_height/2])
-    textRect.center = (display_width/2), (display_height/2)
-    gameDisplay.blit(textSurf, textRect)
+    if cell is 0:
+        textRect.center = (display_width / 2), (display_height / 2)
+    elif cell <= 3 and cell >= 1:
+        textRect.center = (((cell_width * ((cell - 1) % 3)) + (cell_width / 2)), (cell_height / 2))
+    elif cell <= 6 and cell >= 4:
+        textRect.center = (((cell_width * ((cell - 1) % 3)) + (cell_width / 2)), (cell_height + (cell_height / 2)))
+    elif cell <= 9 and cell >= 7:
+        textRect.center = (((cell_width * ((cell - 1) % 3)) + (cell_width / 2)), (cell_height + cell_height + (cell_height / 2)))
+    screen.blit(textSurf, textRect)
 
 def GameLoop():
     # gameOver = False
@@ -44,6 +50,10 @@ def GameLoop():
 
         screen.fill(white)
 
+        message_to_screen("Tic", black, 1)
+        message_to_screen("Tac", black, 2)
+        message_to_screen("Trivia", black, 3)
+
         # screen.fill(red, rect=[0, 0, cell_width, cell_height])
         # screen.fill(black, rect=[cell_width, 0, cell_width, cell_height])
         # screen.fill(red, rect=[cell_width*2, 0, cell_width, cell_height])
@@ -55,6 +65,16 @@ def GameLoop():
         # screen.fill(red, rect=[0, cell_height*2, cell_width, cell_height])
         # screen.fill(black, rect=[cell_width, cell_height*2, cell_width, cell_height])
         # screen.fill(red, rect=[cell_width*2, cell_height*2, cell_width, cell_height])
+        #
+        # message_to_screen("T", white, 1)
+        # message_to_screen("I", white, 2)
+        # message_to_screen("C", white, 3)
+        # message_to_screen("T", white, 4)
+        # message_to_screen("A", white, 5)
+        # message_to_screen("C", white, 6)
+        # message_to_screen("T", white, 7)
+        # message_to_screen("O", white, 8)
+        # message_to_screen("E", white, 9)
 
         pygame.display.update()
 
