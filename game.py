0060e0139d5d967633c014f3643e89842587eb53
XtraRyce <nuevo.matt@gmail.com>
2017-09-24 07:37:33 +0800

    Added cell_button which makes a cell-sized button using parameters

--- game.py
+++ game.py
@@ -16,9 +16,10 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 red = (155, 0, 0)
 green = (0, 155 ,0)
+bright_red = (255,0, 0)
 
-display_width = 1920
-display_height = 1080
+display_width = 1280
+display_height = 720
 cell_width = round(display_width/3)
 cell_height = round(display_height/3)
 
@@ -67,6 +68,17 @@ def get_cell(cell):
         maxY = cell_height * 3
     return (minX, maxX, minY, maxY)
 
+def cell_button(text, cell, fc, fs, ac, ic):
+    mouse = pygame.mouse.get_pos()
+    bounds = get_cell(cell)
+    print(bounds)
+    if bounds[1] >= mouse[0] >= bounds[0] and bounds[3] >= mouse[1] >= bounds[2]:
+        screen.fill(ac, rect=[bounds[0], bounds[2], cell_width, cell_height])
+    else:
+        screen.fill(ic, rect=[bounds[0], bounds[2], cell_width, cell_height])
+    message_to_screen(text, fc, cell, fs)
+
+
 def GameLoop():
     # gameOver = False
     gameExit = False
@@ -78,21 +90,20 @@ def GameLoop():
             message_to_screen("TIC", black, 1, size="large")
             message_to_screen("TAC", black, 2, size="large")
             message_to_screen("TRIVIA", black, 3, size="large")
-            screen.fill(red, rect=[0, cell_height, cell_width, cell_height])
-            screen.fill(red, rect=[cell_width, cell_height, cell_width, cell_height])
-            screen.fill(red, rect=[cell_width*2, cell_height, cell_width, cell_height])
-            screen.fill(red, rect=[cell_width, cell_height*2, cell_width, cell_height])
-            message_to_screen("New Game", white, 4, size="medium")
-            message_to_screen("Demo", white, 5, size="medium")
-            message_to_screen("Options", white, 6, size="medium")
-            message_to_screen("Quit", white, 8, size="medium")
+
+            cell_button("New Game", 4, white, "medium", bright_red, red)
+            cell_button("Demo", 5, white, "medium", bright_red, red)
+            cell_button("Options", 6, white, "medium", bright_red, red)
+            cell_button("Quit", 8, white, "medium", bright_red, red)
 
             print(event)
+
+
             if event.type == pygame.MOUSEBUTTONUP:
                 pos = pygame.mouse.get_pos()
-                quitBounds = get_cell(8)
-                if pos[0] >= quitBounds[0] and pos[0] <= quitBounds[1]:
-                    if pos[1] >= quitBounds[2] and pos[1] <= quitBounds[3]:
+                if quitBounds[1] >= pos[0] >= quitBounds[0] and quitBounds[3] >= pos[1] >= quitBounds[2]:
+                # if pos[0] >= quitBounds[0] and pos[0] <= quitBounds[1]:
+                #     if pos[1] >= quitBounds[2] and pos[1] <= quitBounds[3]:
                         gameExit = True
             if event.type == pygame.QUIT:
                 gameExit = True
